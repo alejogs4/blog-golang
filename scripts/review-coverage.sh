5a27543adbe343694a2efd172d9191fd0c63d517
Alejandro Garcia Serna <alejandrogarcia@seven4n.com>
2021-02-26 15:07:14 -0500

    Add coverage report to docker file test

--- scripts/review-coverage.sh
+++ scripts/review-coverage.sh
@@ -1,2 +1,2 @@
-go test ./... -coverprofile coverage.out
+go test -run Unit ./... -coverprofile coverage.out
 go tool cover -func coverage.out | grep total | awk '{print substr($3, 1, length($3)-1)}'
